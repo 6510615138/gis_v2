aab140d520eebf982521a37f8e958416c735f45e
rnakin <r.nakinton@gmail.com>
2025-06-17 09:56:13 +0700

    Add map loader leaflet

--- gis_v2/settings.py
+++ gis_v2/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Page',
+    'MapPage',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "USER": "gis",
     }
 }
 
