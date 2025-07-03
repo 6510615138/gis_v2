b4a4720b3ff12ea78eb66e53bbddcae96ef36dd1
rnakin <r.nakinton@gmail.com>
2025-07-03 10:48:38 +0700

    add factory_type model

--- RESTapi/models.py
+++ RESTapi/models.py
@@ -8,6 +8,102 @@
 from django.db import models
 
 
+class MappageDistrict(models.Model):
+    code = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+    province_code = models.PositiveSmallIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'MapPage_district'
+
+
+class MappageProvince(models.Model):
+    code = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        managed = False
+        db_table = 'MapPage_province'
+
+
+class MappageSubdistrict(models.Model):
+    code = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+    district_code = models.PositiveSmallIntegerField()
+    is_island = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'MapPage_subdistrict'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    first_name = models.CharField(max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class District(models.Model):
     code = models.AutoField(primary_key=True)
     name = models.TextField()
@@ -16,6 +112,50 @@ class District(models.Model):
     class Meta:
         managed = False
         db_table = 'district'
+
+
+class DjangoAdminLog(models.Model):
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    action_time = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class Factory(models.Model):
@@ -94,6 +234,19 @@ class FactoryTemp1(models.Model):
     class Meta:
         managed = False
         db_table = 'factory_temp (1)'
+
+
+class FactoryType(models.Model):
+    code = models.TextField(primary_key=True)
+    num = models.TextField(blank=True, null=True)
+    type = models.TextField(blank=True, null=True)
+    class1 = models.TextField(blank=True, null=True)
+    class2 = models.TextField(blank=True, null=True)
+    class3 = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'factory_type'
 
 
 class Province(models.Model):
